4ce8178a632a396b57c0cf28291eeb5aedbef1a3
Antbal-ega <39810565+Antbal-ega@users.noreply.github.com>
2018-11-19 11:55:51 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mysql = require('mysql');
+var port = 8080;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/homepage.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
